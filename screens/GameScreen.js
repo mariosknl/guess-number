c6cdba49562d92e5a82ed0fe31b6bb0915819fd1
mariosknl <mariosknl@outlook.com>
2022-03-14 17:24:53 +0200

    using the Dimensions API from react-native

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,12 @@
 import React, { useState, useRef, useEffect } from "react";
-import { Alert, View, Text, ScrollView, StyleSheet } from "react-native";
+import {
+  Alert,
+  Dimensions,
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+} from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 import NumberContainer from "../components/NumberContainer";
@@ -98,13 +105,13 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
-    marginTop: 20,
+    marginTop: Dimensions.get("window").height > 600 ? 20 : 5,
     width: 400,
     maxWidth: "90%",
   },
   listContainer: {
-    width: "80%",
     flex: 1,
+    width: Dimensions.get("window").width > 350 ? "60%" : "80%",
   },
   list: {
     flexGrow: 1,
@@ -119,7 +126,7 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     flexDirection: "row",
     justifyContent: "space-around",
-    width: "60%",
+    width: "100%",
   },
 });
 
